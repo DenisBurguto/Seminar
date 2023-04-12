c9e0cf50c05fe9dce037b3198ea82bf1fad2b69d
«Denis <6545158@gmail.com>
2023-04-12 17:42:00 +0300

    Home task2 done

--- Hometesk2.py
+++ Hometesk2.py
@@ -18,12 +18,17 @@
 
 # Задача 12: Петя и Катя – брат и сестра. Петя – студент, а Катя – школьница. Петя помогает Кате по математике. Он задумывает два натуральных числа X и Y (X,Y≤1000),
 # а Катя должна их отгадать. Для этого Петя делает две подсказки. Он называет сумму этих чисел S и их произведение P. Помогите Кате отгадать задуманные Петей числа.
-
-sum= int(input("Please input summ of of two natural numbers : "))
-mult= int(input("Please input multiply of two natural numbers : "))
-#using Vietta theorem, num1 =(sum +sqrt(sum*sum -4*mult))/2 and num2 = (sum -sqrt(sum*sum -4*mult))/2
-print(f"Your hidden numbers are {int((sum +(sum*sum -4*mult)**0.5)/2)} and {int((sum -(sum*sum -4*mult)**0.5)/2)}")
-
+#
+# sum= int(input("Please input summ of of two natural numbers : "))
+# mult= int(input("Please input multiply of two natural numbers : "))
+# #using Vietta theorem, num1 =(sum +sqrt(sum*sum -4*mult))/2 and num2 = (sum -sqrt(sum*sum -4*mult))/2
+# print(f"Your hidden numbers are {int((sum +(sum*sum -4*mult)**0.5)/2)} and {int((sum -(sum*sum -4*mult)**0.5)/2)}")
 
 
 # Задача 14: Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
+
+num_limit = int(input("Please input the limit number : "))
+degree = 1
+while 2 ** degree <= num_limit:
+    print(f"2 in degree {degree} = {2 ** degree}")
+    degree += 1

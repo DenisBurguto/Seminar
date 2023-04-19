d048e8f236a227696676dab27c3c0bdd960e182f
«Denis <6545158@gmail.com>
2023-04-19 22:25:13 +0300

    additional

--- additional_tasks.py
+++ additional_tasks.py
@@ -11,18 +11,28 @@ def task1():
     random_list = [random.randint(-10, 10) for _ in range(10)]
     print(random_list)
 
-    local_max_ind = len(random_list) - 1
     if random_list[-1] > random_list[-2]:
-        print(local_max_ind)
+        print(f'last local maximum  is {random_list[-1]} with index {len(random_list)-1}')
     else:
-        for i in range(len(random_list) - 2, 1, -1):
-            if random_list[i] > random_list[local_max_ind] and random_list[i] > random_list[i - 1]:
-                local_max_ind = i
-                print(f'last local maximum  is {random_list[i]} with index {i}')
+        for ind in range(len(random_list) - 2, 1, -1):
+            if random_list[ind] > random_list[ind+1] and random_list[ind] > random_list[ind - 1]:
+                print(f'last local maximum  is {random_list[ind]} with index {ind}')
                 break
-
-
 task1()
+
+# local_max = 0
+# N= int(input('Введите количество элементов списка: '))
+# spisok = []
+#
+# for i in range (0,N):
+#     spisok.append(random. randint (0, 10))
+# print(spisok)
+#
+# for i in range(1, len (spisok) -1):
+#     if spisok[i-1] < spisok[i]> spisok[i+1]:
+#             local_max = i
+# print(f' Номер ПОСЛЕДНЕГО локального максимума - {local_max+1} и он равен {spisok[local_max]}')
+
 
 # 2. Создайте список из случайных чисел.
 # Найдите максимальное количество его одинаковых элементов.

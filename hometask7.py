29feadc94c0b716cb0a2af3bd6d710dd481b3e98
«Denis <6545158@gmail.com>
2023-05-20 20:03:54 +0300

    task 36 done

--- hometask7.py
+++ hometask7.py
@@ -12,18 +12,16 @@
 #     **Вывод:** Парам пам-пам
 
 
-def check_rithm(poem_string, test_vowels_set):
-    poem_list = poem_string.lower().split()
-    # filter vowels, get length of vowels string in each phrase, add it to set -
-    # if any different length's the final set length will be more than one:
-    return len(set(map(lambda s: len(list(filter(lambda l: l in test_vowels_set, s))), poem_list))) == 1
-
-
-poem = input('Please enter your poem in russian language :') #ex: пара-ра-рам рам-пам-папам па-ра-па-да
-russian_vowels = {'а', 'о', 'у', 'и', 'ы', 'э', 'е', 'ё', 'ю', 'я'}
-print("Парам пам-пам" if check_rithm(poem, russian_vowels) else "Пам парам")
-
-
+# def check_rithm(poem_string, test_vowels_set):
+#     poem_list = poem_string.lower().split()
+#     # filter vowels, get length of vowels string in each phrase, add it to set -
+#     # if any different length's the final set length will be more than one:
+#     return len(set(map(lambda s: len(list(filter(lambda l: l in test_vowels_set, s))), poem_list))) == 1
+#
+#
+# poem = input('Please enter your poem in russian language :') #ex: пара-ра-рам рам-пам-папам па-ра-па-да
+# russian_vowels = {'а', 'о', 'у', 'и', 'ы', 'э', 'е', 'ё', 'ю', 'я'}
+# print("Парам пам-пам" if check_rithm(poem, russian_vowels) else "Пам парам")
 
 
 # Задача 36: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6), которая принимает в качестве
@@ -34,11 +32,26 @@ print("Парам пам-пам" if check_rithm(poem, russian_vowels) else "Па
 # *Пример:*
 #
 # **Ввод:** `print_operation_table(lambda x, y: x * y) `
+
 # **Вывод:**
 # 1 2 3 4 5 6
-#
 # 2 4 6 8 10 12
 # 3 6 9 12 15 18
 # 4 8 12 16 20 24
 # 5 10 15 20 25 30
 # 6 12 18 24 30 36
+
+
+def print_operation_table(operation, num_rows, num_columns):
+    rows = list(x for x in range(1, num_rows + 1))
+    columns = list(y for y in range(1, num_columns + 1))
+
+    for el in rows:
+        for tel in columns:
+            print(*list(map(operation, [el], [tel])), end=' ')
+        print()
+
+
+operation = lambda x, y: x*y
+num_rows, num_columns = 6, 6
+print_operation_table(operation, num_rows, num_columns)
